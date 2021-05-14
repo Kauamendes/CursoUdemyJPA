b92ae24f73d98e6c644656214dc234f2fd37baf5
Kauã Mendes <kauamendes80@hotmail.com>
2021-05-14 14:05:29 -0300

    Entity user e Resources

--- src/main/java/com/projeto/udemy/CursoUdemyApplicationMAIN.java
+++ src/main/java/com/projeto/udemy/CursoUdemyApplicationMAIN.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoUdemyApplication {
+public class CursoUdemyApplicationMAIN {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursoUdemyApplication.class, args);
+		SpringApplication.run(CursoUdemyApplicationMAIN.class, args);
 	}
 
 }
